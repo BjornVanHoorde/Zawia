6e9b018329aa629fcdd3ac75d36b23cad4c06825
BjornVanHoorde <bjornvanhoorde@hotmail.be>
2022-12-22 08:54:03 +0100

    🔧 Contact Form is working now

---
+++ config/contact-form.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'toEmail'             => 'bjorvanh1@student.arteveldehs.be',
+    'prependSubject'      => '',
+    'prependSender'       => '',
+    'allowAttachments'    => false,
+    'successFlashMessage' => 'Aanvraag verzonden'
+];
